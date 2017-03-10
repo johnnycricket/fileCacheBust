44113569b2e3c59941d04ffef63cdc356b5556cf
John Vorwald <john.vorwald@dice.com>
2017-03-10 10:27:38 -0600

    scaffolding out factories.

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 'use strict';
+
 var fs = require('fs');
 var path = require('path');
+var regexFactory = require('./lib/regexFactory.js');
+var hashGen = require('./lib/hashGenFactory.js');
 
-exports.FileCacheBust = (path, file, matchArray, options) => {
-    
+exports.FileCacheBust = (fileAndPath, matchArray, options) => {
+
+    //parse file for matchers in matchArray, replace with new rel and hash
+
+    //hash gen
+
+    //write file
+
+    //close file
+
+    //error handling
 }
